3ca1065bc812f7fc52375fe4be82dbb359f32e62
Gleb Grishakov <glebson1988@yandex.ru>
2020-04-05 13:28:20 +0300

    add uploading images functionality to production

---
+++ config/initializers/carrier_wave.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET'],
+      region: 'us-east-2'
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+end
